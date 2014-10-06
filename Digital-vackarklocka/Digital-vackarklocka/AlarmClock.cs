13d15859e36af1b6ebec5c75bb04228bfc49369e
unknown <vr222bt@student.lnu.se>
2014-10-06 15:20:20 +0200

    Added fields and properyies to AlarmClock class

--- Digital-vackarklocka/Digital-vackarklocka/AlarmClock.cs
+++ Digital-vackarklocka/Digital-vackarklocka/AlarmClock.cs
@@ -8,5 +8,48 @@ namespace Digital_vackarklocka
 {
     public class AlarmClock
     {
+        //Deklarerar fält
+        private int _alarmHour;
+        private int _alarmMinute;
+        private int _hour;
+        private int _minute;
+
+        public int AlarmHour
+        {
+            get { return _alarmHour; }
+            set
+            {
+                _alarmHour = value;
+            }
+        }
+
+        public int AlarmMinute
+        {
+            get { return _alarmMinute; }
+            set
+            {
+                _alarmMinute = value;
+            }
+        }
+
+        public int Hour
+        {
+            get { return _hour; }
+            set
+            {
+                _hour = value;
+            }
+        }
+
+        public int Minute
+        {
+            get { return _minute; }
+            set
+            {
+                _minute = value;
+            }
+        }
+
+
     }
 }
